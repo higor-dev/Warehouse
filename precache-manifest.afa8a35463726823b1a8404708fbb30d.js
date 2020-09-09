36da9a8626b2962c47ae56dee94667f012fac384
higor-dev <madhigor@hotmail.com>
2020-09-09 20:26:42 -0300

    Updates

---
+++ precache-manifest.afa8a35463726823b1a8404708fbb30d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfc38f540e9d526afa68bd1f9e5cdeed",
+    "url": "/react-store/index.html"
+  },
+  {
+    "revision": "da51a8780d2180ed7e43",
+    "url": "/react-store/static/css/main.6233df15.chunk.css"
+  },
+  {
+    "revision": "402588f0add78686eb22",
+    "url": "/react-store/static/js/2.77b1440c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-store/static/js/2.77b1440c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da51a8780d2180ed7e43",
+    "url": "/react-store/static/js/main.eaa6a927.chunk.js"
+  },
+  {
+    "revision": "439e3e04d79c572b4fdd",
+    "url": "/react-store/static/js/runtime-main.777764d0.js"
+  },
+  {
+    "revision": "849a64433f3bdd08d38623156960aa56",
+    "url": "/react-store/static/media/dogs.849a6443.svg"
+  },
+  {
+    "revision": "dc4429925d170a392d67275b0e5f43b2",
+    "url": "/react-store/static/media/usuario.dc442992.svg"
+  }
+]);
