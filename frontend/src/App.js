6889344cfb37b38119cb0417552638087c88a49d
higor-dev <madhigor@hotmail.com>
2020-09-11 01:57:37 -0300

    Adding protected routes and a custom fetch hook.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import Footer from './Components/Footer';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
 import { UserStorage } from './UserContext';
+import User from './Components/User/User';
+import ProtectedRouter from './Components/Helper/ProtectedRouter';
 
 function App() {
   return (
@@ -16,6 +18,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login/*" element={<Login />} />
+            <ProtectedRouter path="/conta/*" element={<User />} />
           </Routes>
           <Footer />
         </UserStorage>
