5e57688e1449713587dd142689c0e0a48d9b83a8
higor-dev <madhigor@hotmail.com>
2020-09-12 16:16:06 -0300

    Added user post functionality.

--- frontend/src/Components/User/UserPhotoPost.js
+++ frontend/src/Components/User/UserPhotoPost.js
@@ -1,7 +1,79 @@
 import React from 'react';
+import { PHOTO_POST } from '../../api';
+import useFetch from '../../Hooks/useFetch';
+import useForm from '../../Hooks/useForm';
+import Button from '../Forms/Button';
+import Input from '../Forms/Input';
+import styles from './UserPhotoPost.module.css';
+import Error from '../Helper/Error';
+import { useNavigate } from 'react-router-dom';
 
 const UserPhotoPost = () => {
-  return <div></div>;
+  const nome = useForm();
+  const peso = useForm('number');
+  const idade = useForm('number');
+  const [img, setImg] = React.useState({});
+  const { data, error, loading, request } = useFetch();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    if (data) navigate('/conta');
+    console.log(data);
+  }, [data, navigate]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('img', img.raw);
+    formData.append('nome', nome.value);
+    formData.append('peso', peso.value);
+    formData.append('idade', idade.value);
+    console.log(formData);
+
+    const token = window.localStorage.getItem('token');
+    const { url, options } = PHOTO_POST(formData, token);
+    request(url, options);
+  }
+
+  function handleImgChange({ target }) {
+    setImg({
+      preview: URL.createObjectURL(target.files[0]),
+      raw: target.files[0],
+    });
+  }
+
+  return (
+    <>
+      <section className={`${styles.photoPost} animeLeft`}>
+        <form onSubmit={handleSubmit}>
+          <Input label="Produto" type="text" name="nome" {...nome} />
+          <Input label="Quantidade" type="number" name="peso" {...peso} />
+          <Input label="Preço" type="number" name="idade" {...idade} />
+          <input
+            className={styles.file}
+            type="file"
+            name="img"
+            id="img"
+            onChange={handleImgChange}
+          />
+          {loading ? (
+            <Button disabled>Enviando...</Button>
+          ) : (
+            <Button>Enviar</Button>
+          )}
+          <Error error={error} />
+        </form>
+        <div>
+          {img.preview && (
+            <div
+              className={styles.preview}
+              style={{ backgroundImage: `url('{ ${img.preview}}')` }}
+            ></div>
+          )}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default UserPhotoPost;
--- frontend/src/Hooks/useForm.js
+++ frontend/src/Hooks/useForm.js
@@ -5,6 +5,10 @@ const types = {
     regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: 'Preencha um email válido',
   },
+  number: {
+    regex: /^\d+$/,
+    message: 'Utilize apenas números.',
+  },
 };
 
 const useForm = (type) => {
