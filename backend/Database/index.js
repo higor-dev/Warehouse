bacd5bdd3acfe1df4685a5ce12ff79836317c5bf
fabio alves <fabio.alves@exablack.com>
2020-09-14 10:57:17 -0300

    added: the start of user controller and database log removed

--- backend/Database/index.js
+++ backend/Database/index.js
@@ -24,7 +24,7 @@ const model = require('./model');
 
 
 (async () => {
-  await sequelize.sync({force:true});
+  await sequelize.sync();
 })();
 
 
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,12 @@
-const express = require('express');
 require('dotenv/config');
-const { db_connection } = require("./Database/index.js")
+const express = require('express');
 const app = express();
 const port = process.env.PORT;
+const router = require("./Controller/userController");
 
-
-app.get("/", (request, response) => {
-	const jane =
-	response.send("Hello, world");
-})
+app.use(express.urlencoded({extended: true})) 
+app.use(express.json())
+app.use(router);
 
 app.listen(port, () => {
 	console.log(`Connected to port ${port}`);
