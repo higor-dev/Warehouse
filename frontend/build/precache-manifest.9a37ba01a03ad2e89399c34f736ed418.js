5ca8dd8cfb7e04ac113c54e31675836ae559b88b
higor-dev <madhigor@hotmail.com>
2020-09-14 22:41:15 -0300

    Deploying to Github Pages

---
+++ frontend/build/precache-manifest.9a37ba01a03ad2e89399c34f736ed418.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e47630b2bdd4977eede489e62ef6a1c",
+    "url": "/react-store/index.html"
+  },
+  {
+    "revision": "30196fc555211c8d8a94",
+    "url": "/react-store/static/css/main.104330bf.chunk.css"
+  },
+  {
+    "revision": "500f9371ca24a776b542",
+    "url": "/react-store/static/js/2.eb30bdfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-store/static/js/2.eb30bdfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30196fc555211c8d8a94",
+    "url": "/react-store/static/js/main.74505073.chunk.js"
+  },
+  {
+    "revision": "439e3e04d79c572b4fdd",
+    "url": "/react-store/static/js/runtime-main.777764d0.js"
+  },
+  {
+    "revision": "c6e243c75ebc97ddc04eb362ec882b0b",
+    "url": "/react-store/static/media/adicionar.c6e243c7.svg"
+  },
+  {
+    "revision": "5ce6ac31ceefce525e7de75fb3298f1a",
+    "url": "/react-store/static/media/estatisticas.5ce6ac31.svg"
+  },
+  {
+    "revision": "95db747853a588581330f83bd79bea07",
+    "url": "/react-store/static/media/feed.95db7478.svg"
+  },
+  {
+    "revision": "efa03022cd66ca19df390c3ffdc421ec",
+    "url": "/react-store/static/media/sair.efa03022.svg"
+  },
+  {
+    "revision": "90bd3ec8c1969710d43fef44d68566d5",
+    "url": "/react-store/static/media/tool.90bd3ec8.svg"
+  },
+  {
+    "revision": "78901e1fff162801f63d4edba671cbad",
+    "url": "/react-store/static/media/usuario.78901e1f.svg"
+  }
+]);
