bf9c08c2bd0672c888d44a47b411cd1462392e48
higor-dev <madhigor@hotmail.com>
2020-09-17 20:39:16 -0300

    Loading animation for Home and Products Page

--- frontend/src/Components/Helper/Loading.js
+++ frontend/src/Components/Helper/Loading.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import styles from './Loading.module.css';
 
 const Loading = () => {
-  return <div>Carregando...</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.loading}></div>.
+    </div>
+  );
 };
 
 export default Loading;
--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { UserContext } from '../UserContext';
 import Feed from './Feed/Feed';
+import Loading from './Helper/Loading';
 
 const Home = () => {
+  const { loading } = React.useContext(UserContext);
   return (
     <section className="container mainContainer">
+      {loading && <Loading />}
       <Feed />
     </section>
   );
