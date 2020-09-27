3fce1cb3a75222da116f358a8a94090453b7cf1d
higor-dev <madhigor@hotmail.com>
2020-09-26 22:10:37 -0300

    hotfix

--- backend/index.js
+++ backend/index.js
@@ -13,10 +13,10 @@ const bcrypt = require('bcrypt');
 const { db_connection } = require('./Database/index');
 var cors = require('cors');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(express.json());
 app.use(cors());
 app.use(router);
 app.use(product);
